354e734b67dfff9c57b6f849af22370f2fb14511
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-02-04 17:17:57 -0600

    Created the AdminView component that is used to handle the Admin component request and state

--- donor_management/src/views/AdminView.js
+++ donor_management/src/views/AdminView.js
@@ -5,10 +5,44 @@ import Admin from '../components/Admin';
 import { addNewDonor } from '../store/actions';
 
 class AdminView extends React.Component {
+    state = {
+        donorName: '',
+        contactInfo: {
+            phoneNumber:'',
+            email: ''
+        },
+        pastDonations: [],
+        locationOfDonation: '',
+        dateOfCommunication: '',
+        methodOfCommunication: ''
+    }
+
+    handleInput = e => {
+        e.preventDefault();
+        this.setState({ [e.target.name]: e.target.value });
+    }
+    addNewDonor = e => {
+        e.preventDefault();
+        const newDonor = { 
+            donorName: this.state.donorName, 
+            contactInfo: {phoneNumber: this.state.phoneNumber, email: this.state.email},
+            pastDonations: this.state.pastDonations,
+            locationOfDonation: this.state.locationOfDonation,
+            methodOfCommunication: this.state.methodOfCommunication
+        }
+        this.props.addNewDonor(newDonor);
+    }
+
     render(){
         return (
             <div>
-
+                <Admin 
+                    donorName={this.state.donorName} 
+                    contactInfo={this.state.contactInfo} 
+                    dateOfCommunication={this.state.dateOfCommunication} 
+                    methodOfCommunication={this.state.methodOfCommunication}
+                    handleInput={this.handleInput}
+                />
             </div>
         );
     }
