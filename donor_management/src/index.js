ee762b3ef761ac654787b2a60fa41b55325437e6
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-02-04 11:53:15 -0600

    set up index file to use react router and redux

--- donor_management/src/index.js
+++ donor_management/src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import rootReducer from "./store/reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk, logger)
+  );
+
+  ReactDOM.render(
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>,
+    document.getElementById("root")
+  );
