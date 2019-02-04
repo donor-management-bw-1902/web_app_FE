3b4de5a7483718da1455bb80b50001de246e2216
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-02-04 12:58:38 -0600

    Created a loginView used to handle the login of the user/boardmember

--- donor_management/src/views/LoginView.js
+++ donor_management/src/views/LoginView.js
@@ -1,1 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { Login } from '../components';
+
+class LoginView extends React.Component {
+    state = {
+        username: '',
+        password: ''
+    };
+
+    handleLogin = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    login = () => {
+
+    }
+    
+    render() {
+        return(
+            <div>
+                <Login />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    users: state.userReducer.users,
+    userId: state.userReducer.userId
+});
+
+export default connect(
+    mapStateToProps,
+    {}
+)(LoginView);
